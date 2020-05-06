9f2bde547862fd414daec42d99a5c260cb530498
dayvson <dayvsondossantossilva@gmail.com>
2020-05-06 10:04:36 -0400

    function split

--- get_inf_page.py
+++ get_inf_page.py
@@ -10,7 +10,7 @@ import networkx as nx
 
 # https://scholar.google.com.br/citations?hl=pt-BR&user=OXYNcyoAAAAJ
 
-def make_html():
+def read_page_perfil():
     page = ""
     with open("fernanda_pires.html", "r") as fl:
         for line in fl:
@@ -25,55 +25,70 @@ def request_page():
             page = requests.get(url)
             return page
         except:
-            print("Invalid Request, try again in 10s")
+            print("Invalid Request, trying again in 10s")
             sleep(10)
 
-def clean_authors(authors):
-    authors = list(map(lambda b: b.replace(" FG de Sousa Pires","F Pires"), authors))
+def rename_authors(authors):
     authors = list(map(lambda b: b.replace("FG de Sousa Pires","F Pires"), authors))
     authors = list(map(lambda b: b.replace("KB Teixeira","K Teixeira"), authors))
-    authors = list(map(lambda b: b.replace(" K Teixeira","K Teixeira"), authors))
     return authors
 
-if __name__ == "__main__":
-  
-    # page = request_page()
-    page = make_html()
-    soup = BeautifulSoup(page,  "html.parser")
+def get_year(result, paper_name):
+    try:
+        year = int(result.contents[2].contents[0].contents[0])
+        return year
+    except:
+        year = 0
+        print("The paper:", paper_name, ", no has year")
+        return year
 
-    # Creating graph
-    G = nx.Graph()
-    #G1 = nx.Graph()
-
-    ## Extracting info of page
-    results = soup.find_all(class_="gsc_a_tr")
-    p = "A"
-    n = 1
+def extract_inf_perfil(results):
+    paper_graph = {}
+    # p, n = "A", 1
     for result in results:
         paper_name = result.contents[0].contents[0].contents[0]
         authors = str(result.contents[0].contents[1].contents[0]).split(", ")
-        try:
-            year = int(result.contents[2].contents[0].contents[0])
-        except:
-            year = 0
-            print("The paper:", paper_name, ", no has year")
-
+        year = get_year(result, paper_name)
+        authors = rename_authors(authors)
         if year >= 2017:
-            paper_number = p + str(n)
-            n += 1
-            
-            authors = clean_authors(authors)
+            # paper_number = p + str(n)
+            # n += 1 
+            paper_graph[paper_name] = authors
 
-            print(authors)
+    # pprint(paper_graph)
+    return paper_graph
 
-            # creanting edges
-            for author in authors:
-                G.add_edge(paper_name, author)
-                #G1.add_edge(paper_number, author)
+def generate_authors_graph(graph):
+    # Creating graph
+    G = nx.Graph()
 
+    # creanting edges
+    for paper in graph.keys():
+        for author in graph[paper]:
+            G.add_edge(paper, author)
+
+    # define size image
+    plt.figure(figsize=(50, 70))
     nx.draw(G, with_labels=True)
-    # nx.draw(G1, with_labels=True)
-    plt.savefig("artigos_com_nome.png") # save as png
-    #plt.savefig("artigos_com_sigla.png") # save as png
+    # plt.savefig("artigos_com_nome.png") # save as png
+    # plt.savefig("artigos_com_sigla.png") # save as png
     plt.show() # display
-    ## Extracting all author names:
+
+def main():
+    # online
+    # page = request_page()
+    # offline
+    page = read_page_perfil()
+
+    soup = BeautifulSoup(page,  "html.parser")
+
+    results = soup.find_all(class_="gsc_a_tr")
+
+    g = extract_inf_perfil(results)
+
+    generate_authors_graph(g)
+
+
+if __name__ == "__main__":
+    main()
+    
